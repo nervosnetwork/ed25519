bdd078ead9f3850e6cf9ca4e97775d83b081578d
xjd <xjd@cryptape.com>
2021-03-25 15:34:25 +0800

    new test vector for polkadot

--- run-tests.sh
+++ run-tests.sh
@@ -6,9 +6,16 @@ cd build.simulator
 cmake -DCMAKE_C_COMPILER=clang ..
 make all
 
-cat ../tests/TESTVECTORS | while read line || [[ -n $line ]];
-do
-   ./portable_ed25519 -t ${line//:/ 0x}
-done
+function test_one() {
+  echo "test vector from $1"
+  FN=$1
+  cat $FN | while read line || [[ -n $line ]];
+  do
+     ./portable_ed25519 -t ${line//:/ 0x}
+  done
+}
+
+test_one ../tests/TESTVECTORS
+test_one ../tests/polkadot.txt
 
 echo "if no output from stdout, it means success!"
